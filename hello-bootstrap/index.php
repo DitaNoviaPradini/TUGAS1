b6970ab280034ed86eb1c76daf38d4fe44877f69
DitaNoviaPradini <diditapradini40@gmail.com>
2018-04-05 19:28:26 +0700

    first commit

---
+++ hello-bootstrap/index.php
@@ -0,0 +1,12 @@
+<!DoCTYPE html>
+<html>
+<head>
+<title> Hello Bootstrap </title>
+<h1 class="text-center">Hello Bootstrap</h1>
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
